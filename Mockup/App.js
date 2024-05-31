db7a0b83de1d45d99f38c7da689a702c9ac97fa3
StephenAgyemang <StephenAsamoahAgyemang@outlook.com>
2024-05-31 10:45:25 +0000

    I included 6 more tasks

--- Mockup/App.js
+++ Mockup/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image, TouchableOpacity } from 'react-native';
+import Categories from './components/categories';
 
 export default function App() {
   return (
@@ -9,71 +10,119 @@ export default function App() {
           <Text style={styles.header}>Hello Dev's</Text>
           <Text style={styles.miniheader}>14 tasks today</Text>
         </View>
-        <View style={styles.profileImg}></View>
+        <View style={styles.profileImg}>
+          <Image style={styles.img} source={require("./img/download.png")} />
+        </View>
       </View>
-      <TextInput type='text' placeholder='Search' style={styles.searchbar} />
+
+      <View style={styles.stylesContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search"
+        />
+
+       <TouchableOpacity>
+        <Image
+          source={require("./img/Group 2.svg")} 
+          style={styles.settings}
+        /> 
+        </TouchableOpacity> 
+      </View>
+
       <Text style={styles.subheader}>Categories</Text>
-      <ScrollView>
-        <view style={styles.categoriescard}></view>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <Categories imagesrc={require('./img/young woman working online.svg')} header={'Exercise'} tasks={'12 tasks'} />
+        <Categories imagesrc={require('./img/3d-hygge-isometric-view-of-young-woman-sorting-craft-paper-2.png')} header={'Study'} tasks={'12 tasks'} />
+        <Categories imagesrc={require('./img/3d-hygge-isometric-view-of-young-woman-and-man-discussing-the-project.png')} header={'Work'} tasks={'12 tasks'} />
+        <Categories imagesrc={require('./img/3d-hygge-isometric-view-of-young-woman-leaning-on-table-2.png')} header={'Cook'} tasks={'12 tasks'} />
+        <Categories imagesrc={require('./img/3d-hygge-isometric-view-of-young-woman-sorting-craft-paper-2.png')} header={'Paint'} tasks={'12 tasks'} />
+        <Categories imagesrc={require('./img/3d-hygge-man-sitting-on-floor-with-laptop.png')} header={''} tasks={'12 tasks'} />
+        <Categories imagesrc={require('./img/3d-hygge-isometric-view-of-young-girl-and-young-man-having-dinner-together-1.png')} header={'Eat'} tasks={'12 tasks'} />
+        <Categories imagesrc={require('./img/3d-hygge-isometric-view-of-man-working-online.png')} header={'Call'} tasks={'12 tasks'} />
       </ScrollView>
       <StatusBar style="auto" />
     </ScrollView>
+
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F7F0E8',
+    paddingTop: '30px',
+    padding: '25px',
+    fontFamily: 'lato',
   },
 
   headerView: {
     display: 'flex',
     flexDirection: 'row',
-    gap: '100px',
     alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: '50px',
+    gap: '135px',
+    marginBottom: '20px'
+
   },
 
   header: {
     fontSize: 32,
     fontWeight: 'bold',
-    fontFamily: 'Lato',
+    
   },
   subheader: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    fontFamily: 'Lato',
+    fontSize: '20px',
+    fontWeight: '700',
+    fontFamily: 'lato',
     marginTop: '30px',
-    marginLeft: '20px'
+    marginBottom:'10px'
   },
 
   miniheader: {
     fontSize: '12px',
     fontFamily: 'Lato',
-    
+
   },
 
-  searchbar: {
-    width: '70%',
-    fontWeight: 'bold',
-    fontSize: '16px',
-    fontFamily: 'Lato',
-    marginTop: '15px',
-    marginLeft: '15px',
-    height: '48px',
-    borderRadius: '20px',
-    paddingLeft: '15px',
-    border: 'solid 1px #FBF9F7',
-  },
+
 
   profileImg: {
-    width: '60px',
-    height: '60px',
-    borderRadius: '20px',
+    width: '50px',
+    height: '50px',
+    borderRadius: '40px',
     backgroundColor: '#fff',
+    alignContent: 'center'
 
-  }
+  },
+
+  img: {
+    height: '45px',
+    width: '46px'
+  },
+
+  settings: {
+    height: '50px',
+    width: '52px'
+
+  },
+
+  stylesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '40px',
+    MarginTop: '100px'
+  },
+
+  searchInput: {
+    flex: 1,
+    height: '48px',
+    width: '200px',
+    backgroundColor: '#fff',
+    border: 'none',
+    borderRadius: '14px',
+    paddingHorizontal: 10,
+  },
+
+
 
 });
 
---
+++ Mockup/components/categories.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native'
+
+const Categories = ({imagesrc, header,tasks }) => {
+  return ( 
+    <View style={styles.categoriescard}>
+      <Text style={styles.catHeader}>{ header }</Text>
+      <Text style={styles.tasks}>{ tasks }</Text>
+      <Image
+          source={ imagesrc } 
+          style={styles.tasksImg}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  categoriescard:{
+    width: '186px',
+    height: '192px',
+    backgroundColor: '#FBF9F7',
+    borderRadius: '15px',
+    padding:'15px',
+    marginRight:'30px'
+  },
+
+  catHeader:{
+    fontWeight: '700',
+    fontSize: '16px',
+  },
+
+  tasks:{
+    fontSize:'12px',
+  },
+
+  tasksImg:{
+    height: '130px',
+    width: '130px',
+    marginLeft:'20px',
+
+  }
+
+
+})
+
+export default Categories;
